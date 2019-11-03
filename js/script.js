eafd76bb2d7a4b9e95c4a2d5b87c44cc3c586905
Vitaly Osipov <vi7ali@gmail.com>
2019-11-03 15:25:20 +0300

    Add final layout and buttons behavior

--- js/script.js
+++ js/script.js
@@ -7,12 +7,32 @@ function changeContent(e) {
 }
 
 function restartGame() {
-    document.getElementById('intro-screen').classList.remove('intro-screen--hide');
+    fadeIn();    
     const gridCells = document.getElementsByClassName('grid__cell');
     for(let gridCell of gridCells) {
         gridCell.textContent = 'Q';
         gridCell.classList.add('grid__cell--hidden');        
     }
+}
+
+function fadeOut() {
+    let playBtn = document.getElementById('play-btn');    
+    let introScreen = document.getElementById('intro-screen');
+    playBtn.style.display = 'none';
+    introScreen.classList.toggle('intro-screen--hidden');
+    setTimeout(function(){
+        introScreen.style.display = 'none';
+    }, 1000);
+}
+
+function fadeIn() {
+    let introScreen = document.getElementById('intro-screen');
+    let playBtn = document.getElementById('play-btn');  
+    playBtn.style.display = 'block';  
+    introScreen.style.display = 'block';     
+    setTimeout(function(){
+        introScreen.classList.toggle('intro-screen--hidden');
+    }, 200);
 }
 
 function render() {
@@ -21,9 +41,7 @@ function render() {
     const gridCells = document.getElementsByClassName('grid__cell');
 
     restartBtn.addEventListener('click', restartGame, false);
-    playBtn.addEventListener('click', 
-                            () => document.getElementById('intro-screen').classList.add('intro-screen--hide'), 
-                            false);
+    playBtn.addEventListener('click', fadeOut, false);
 
     for(let gridCell of gridCells) {
         gridCell.textContent = 'Q';

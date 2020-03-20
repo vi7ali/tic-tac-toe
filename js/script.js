a02c7b938cbc35e7ce08a630887c2951bd8fd1a4
vi7ali <vi7ali@gmail.com>
2020-03-20 12:33:24 +0300

    Add Game Logic

--- js/script.js
+++ js/script.js
@@ -1,59 +1,147 @@
 'use strict'
+const PlayerFactory = (name, mark) => {
+  let playerName = name;
 
-const changeContent = e => {
-    let target = e.target;
-    target.textContent = 'Y';    
+  if (!playerName)
+    playerName = mark === 'X' ? 'Player 1' : 'Player 2';
+
+  const sayHello = () => console.log(`Hi, my name is ${playerName}, mark - ${mark}`);
+
+  return {
+    playerName,
+    sayHello,
+    mark
+  }
 }
 
 const gameUI = (() => {
-    // Private        
-    const playBtn = document.getElementById('play-btn');    
-    const gridCells = document.getElementsByClassName('grid__cell');
-    const introScreen = document.getElementById('intro-screen');
-    const inputNames = document.getElementsByClassName('intro-screen__input');
-    
-    const fadeIn = () => {
-        playBtn.style.display = 'block';  
-        introScreen.style.display = 'block';     
-        setTimeout(function(){
-            introScreen.classList.toggle('intro-screen--hidden');
-        }, 200);
-    }
+  // Private        
+  const playBtn = document.getElementById('play-btn');
+  const introScreen = document.getElementById('intro-screen');
+  //Public
+  const fadeIn = () => {
+    playBtn.style.display = 'block';
+    introScreen.style.display = 'block';
+    setTimeout(function() {
+      introScreen.classList.toggle('intro-screen--hidden');
+    }, 200);
+  }
 
-    const fadeOut = () => {
-        playBtn.style.display = 'none';
-        introScreen.classList.toggle('intro-screen--hidden');
-        setTimeout(function(){
-            introScreen.style.display = 'none';
-        }, 1000);
-    }
-    //Public
-    const redrawUI = (opt) => {
-        for(let gridCell of gridCells) 
-            gridCell.textContent = '';
+  const fadeOut = () => {
+    playBtn.style.display = 'none';
+    introScreen.classList.toggle('intro-screen--hidden');
+    setTimeout(function() {
+      introScreen.style.display = 'none';
+    }, 1000);
+  }
 
-        for(let input of inputNames)
-            input.value = '';
-        console.log(opt.newGame);
-        opt.newGame ? fadeOut() : fadeIn();
-    }
+  return {
+    fadeIn,
+    fadeOut
+  };
+})();
 
-    return { redrawUI }
+const gameLogic = (() => {
+  const gridCells = document.getElementsByClassName('grid__cell');
+  let player1;
+  let player2;
+  let currentPlayer;
+  let winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  const addListeners = () => {
+    for (let gridCell of gridCells)
+      gridCell.addEventListener('click', makeMove, false);
+  }
+
+  const removeListeners = () => {
+    for (let gridCell of gridCells)
+      gridCell.removeEventListener('click', makeMove, false);
+  }
+
+  const newGame = () => {
+    let name1 = document.getElementById('player1-name').value;
+    let name2 = document.getElementById('player2-name').value;
+    player1 = PlayerFactory(name1, 'X');
+    player2 = PlayerFactory(name2, 'O');
+    currentPlayer = player1;    
+    gameUI.fadeOut();
+    addListeners();
+  }
+
+  const restartGame = (() => {
+    for (let gridCell of gridCells)
+      gridCell.textContent = '';
+    gameUI.fadeIn();
+    removeListeners();
+  })
+
+  const makeMove = e => {
+    if (!e.target.innerText) {
+      e.target.innerText = currentPlayer.mark;
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+      currentPlayer.sayHello();
+      checkWin();
+    }
+  }
+
+  const checkWin = () => {
+    let board = [];
+    let gridCells = document.getElementsByClassName('grid__cell');
+    for (let gridCell of gridCells)
+      board.push(gridCell.innerText);
+    // Remove the win combinations that have empty cells
+    // And get the win combination by comparing cells + flat
+    let winCondition = winConditions
+      .filter(condition => board[condition[0]] && board[condition[1]] && board[condition[2]])
+      .filter(win => board[win[0]] === board[win[1]] && board[win[0]] === board[win[2]])
+      .flat();
+    // Tie case    
+    if (!winCondition.length && !board.includes(''))
+      endGame();
+    // Win case
+    if (winCondition.length) {
+      endGame(board[winCondition[0]]);
+    }
+  }
+
+  const endGame = (mark = false) => {
+    removeListeners();
+    if (mark) {
+      let winner = player1.mark == mark ? player1 : player2;
+      gameUI.displayWinner(winner);
+    } else {
+      gameUI.displayTie();
+    }
+  }
+
+  return {
+    newGame,
+    restartGame
+  }
 })();
 
 const gameFlow = (() => {
-    const restartBtn = document.getElementById('restart');
-    const playBtn = document.getElementById('play-btn');    
-    // const gridCells = document.getElementsByClassName('grid__cell');
+  const restartBtn = document.getElementById('restart');
+  const playBtn = document.getElementById('play-btn');
 
-    restartBtn.addEventListener('click', gameUI.redrawUI.bind(this, {newGame: false}), false);
-    playBtn.addEventListener('click', gameUI.redrawUI.bind(this, {newGame: true}), false);           
-    // gridCell.addEventListener('click', changeContent, false);
-    
-    // const init = () => gameUI.redrawUI({newGame: true});
-    
-    // return {init}
+  const init = () => {
+    restartBtn.addEventListener('click', gameLogic.restartGame, false);
+    playBtn.addEventListener('click', gameLogic.newGame, false);
+  };
+
+  return {
+    init
+  };
 })();
 
 
-// gameFlow.init();
+gameFlow.init();
